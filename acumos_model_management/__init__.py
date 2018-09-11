126ddba9f0caa00cd7a90af4d23890d611091c6f
Kazakov, Pavel (pk9069) <pk9069@att.com>
2018-09-10 18:58:08 -0700

    Model Management
    Add basic project structure for the REST interfaces in model manager.
    
    Change-Id: Idfbf510051c41cabffcc194ba232cbf59764a0b5
    Issue-ID: ACUMOS-1226
    Signed-off-by: Kazakov, Pavel (pk9069) <pk9069@att.com>

---
+++ acumos_model_management/__init__.py
@@ -0,0 +1,24 @@
+# ===============LICENSE_START=======================================================
+# Acumos Apache-2.0
+# ===================================================================================
+# Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
+# ===================================================================================
+# This Acumos software file is distributed by AT&T
+# under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# This file is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============LICENSE_END=========================================================
+
+_VERSION = '0.1.0'
+
+def get_version():
+    # method is used instead of calling _VERSION directly in the event we wish to internally change the way version is
+    # stored but still want to do a conversion to make it PEP 440-compliant
+    return _VERSION
