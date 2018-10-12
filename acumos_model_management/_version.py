47bcd39fc89feb557c37b4b42f37b8a0bdf8b7e2
rh1832 <rh1832@att.com>
2018-10-12 21:14:59 +0000

    Model Management Sonar pom
    Adding sonar pom and making version method same across all services
    
    Issue-ID: ACUMOS-1226
    
    Change-Id: Ie4957ed74ed6f3a2a7c23a2ed6e8a8c3d9c23896
    Signed-off-by: rh1832 <rh1832@att.com>

--- acumos_model_management/_version.py
+++ acumos_model_management/_version.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+#
 # ===============LICENSE_START=======================================================
-# Acumos Apache-2.0
+# Acumos
 # ===================================================================================
 # Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
 # ===================================================================================
@@ -9,11 +10,11 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # This file is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============LICENSE_END=========================================================
-from ._version import __version__  # noqa
+__version__ = '0.1.0'
--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
+#!/usr/bin/env python3
+#
 # ===============LICENSE_START=======================================================
-# Acumos Apache-2.0
+# Acumos
 # ===================================================================================
 # Copyright (C) 2018 AT&T Intellectual Property. All rights reserved.
 # ===================================================================================
@@ -8,17 +10,24 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # This file is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ===============LICENSE_END=========================================================
-
+from os.path import dirname, abspath, join as path_join
 from setuptools import setup, find_packages
 
-version = __import__('acumos_model_management').get_version()
+
+SETUP_DIR = abspath(dirname(__file__))
+
+with open(path_join(SETUP_DIR, 'acumos_model_management', '_version.py')) as file:
+    globals_dict = dict()
+    exec(file.read(), globals_dict)
+    __version__ = globals_dict['__version__']
+
 
 with open("README.md") as fd:
     long_description = fd.read()
@@ -50,5 +59,5 @@ setup(
     packages=find_packages(),
     python_requires='>=3.5',
     url='https://gerrit.acumos.org/r/gitweb?p=on-boarding/model-management.git',
-    version=version,
+    version=__version__,
 )
