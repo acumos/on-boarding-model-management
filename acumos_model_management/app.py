05879af1ad19e56d24683187485238d14d88c7e3
gs495a <gs495a@att.com>
2018-10-30 17:01:32 -0500

    ACUMOS MODEL MANAGEMENT DOCKER IMAGE
    ADDED DOCKERFILE AND RESPECTIVE CODE CHAGNES
    
    Issue-ID: ACUMOS-1960
    
    Change-Id: I0aaac36a6bbf3b8fa03bf9a5f2f440fec766fc87
    Signed-off-by: gs495a <gs495a@att.com>

--- acumos_model_management/app.py
+++ acumos_model_management/app.py
@@ -20,11 +20,32 @@ from acumos_model_management import settings
 from acumos_model_management.api.restplus import api_v2, blueprint_v2
 from acumos_model_management.api.v2.endpoints import api as modelmanager_v2_namespace
 from acumos_model_management.database.models import db
+import configparser
+import os
+
+
+def load_properties(config_path, flask_app):
+    parser = configparser.ConfigParser()
+    parser.read(config_path)
+    mongo = parser['MONGO']
+    flask_app.config['MONGODB_SETTINGS'] = {
+        'host': mongo['mongo_host'],
+        'port': mongo.getint('mongo_port'),
+        'db': mongo['mongo_dbname'],
+        'username': mongo['mongo_username'],
+        'password': mongo['mongo_password'],
+    }
+
+    files_config = parser['FILES']
+    flask_app.config['UPLOAD_FOLDER'] = files_config['upload_dir']
 
 
 def initialize_app(flask_app):
     """Initializes the REST interfaces and database"""
     flask_app.config['ERROR_404_HELP'] = settings.ERROR_404_HELP
+
+    dir_path = os.path.dirname(os.path.dirname(os.path.join(os.path.realpath(__file__))))
+    load_properties(os.path.join(dir_path, 'properties', 'settings.cfg'), flask_app)
 
     api_v2.add_namespace(modelmanager_v2_namespace)
     flask_app.register_blueprint(blueprint_v2)
